1d51514b17554954a3a7472338b05728551c4482
KimJeongJin <wjdwls8520@gmail.com>
2026-03-25 14:55:56 +0900

    [Server] 에러핸들러 중복 제거

--- was/src/main/java/com/yogimangchi/domain/community/dto/request/PostCreateRequest.java
+++ was/src/main/java/com/yogimangchi/domain/community/dto/request/PostCreateRequest.java
@@ -2,6 +2,7 @@ package com.yogimangchi.domain.community.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,10 +10,12 @@ import lombok.Setter;
 @Setter
 public class PostCreateRequest {
     @Schema(description = "게시글 제목", example = "오늘 시장 정리")
+    @NotNull
     @NotBlank
     private String title;
 
     @Schema(description = "게시글 내용", example = "내용")
+    @NotNull
     @NotBlank
     private String content;
 }
