13697115a2be015f57225e635d88ea8c3157cd2f
KimJeongJin <wjdwls8520@gmail.com>
2026-03-28 20:22:37 +0900

    [Server] 내가 좋아요한 댓글, 대댓글 한번에 조회하기 api 제작

--- was/src/main/java/com/yogimangchi/domain/community/controller/v1/MyCommunityController.java
+++ was/src/main/java/com/yogimangchi/domain/community/controller/v1/MyCommunityController.java
@@ -2,6 +2,7 @@ package com.yogimangchi.domain.community.controller.v1;
 
 import com.yogimangchi.domain.community.dto.response.PostAndMemberDto;
 import com.yogimangchi.domain.community.dto.response.PostDetailDto;
+import com.yogimangchi.domain.community.dto.response.ReplyDetailDto;
 import com.yogimangchi.domain.community.service.LikeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/me/community")
 @RequiredArgsConstructor
-@Tag(name = "03 - Me-Community-Post", description = "커뮤니티 게시글 관련 API")
+@Tag(name = "03 - Me-Community-Post", description = "내가 좋아요한 글 / 댓글 관련 api")
 public class MyCommunityController {
 
     private final LikeService likeService;
@@ -39,5 +40,20 @@ public class MyCommunityController {
         Page<PostAndMemberDto> likedPosts = likeService.getLikedPosts(loginMemberId, page, size, keyword);
 
         return ResponseEntity.ok(likedPosts);
+    }
+
+    @Operation(
+            summary = "내가 좋아요 누른 모든 댓글, 대댓글 한번에 조회",
+            description = "내가 좋아요 누른 모든 댓글, 대댓글 한번에 조회, 무한 스크롤로 페이징 합니다."
+    )
+    @GetMapping("/liked-replys")
+    public ResponseEntity<Page<ReplyDetailDto>> getLikedReplys(
+            @AuthenticationPrincipal Long loginMemberId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        Page<ReplyDetailDto> likedReplys = likeService.getLikedReplys(loginMemberId, page, size);
+
+        return ResponseEntity.ok(likedReplys);
     }
 }
