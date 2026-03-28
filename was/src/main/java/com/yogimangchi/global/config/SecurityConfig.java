3fef1ec39a61e1e0c18f3e8bc2bd6ef649e24cf3
KimJeongJin <wjdwls8520@gmail.com>
2026-03-28 13:29:22 +0900

    [Server] 코인 심볼 조회하기 시큐리티 수정

--- was/src/main/java/com/yogimangchi/global/config/SecurityConfig.java
+++ was/src/main/java/com/yogimangchi/global/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
                                 "/api/v1/auth/**",
                                 // "/api/v1/chartapi/**",
                                 "/api/v1/member/nickname/duplication",
-                                "/api/v1/trade/**"
+                                "/api/v1/trade/**",
+                                "/api/v1/market/symbols"
                         ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/community/**").permitAll() // 커뮤니티 조회는 비회원 허용
                         .anyRequest().authenticated()
