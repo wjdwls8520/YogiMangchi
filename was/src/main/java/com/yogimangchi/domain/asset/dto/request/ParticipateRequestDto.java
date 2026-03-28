42f3e03ae1cc9be6e31732247fd0fa0e04237979
hangeunyoung <hms09131@gmail.com>
2026-03-28 22:04:08 +0900

    [Server] 동적쿼리 작성은 위한 QueryDSL 환경설치

--- was/src/main/java/com/yogimangchi/domain/asset/dto/request/ParticipateRequestDto.java
+++ was/src/main/java/com/yogimangchi/domain/asset/dto/request/ParticipateRequestDto.java
@@ -4,9 +4,13 @@ import com.yogimangchi.domain.asset.enums.AssetType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "사용자가 거래 콘텐츠(모의투자, 실전 현물/선물, 대회)에 참가할 때 사용하는 요청 DTO")
 public record ParticipateRequestDto(
 
         @NotNull(message = "콘텐츠 타입은 필수입니다.")
-        @Schema(description = "참가할 콘텐츠 타입 (SPOT: 현물, FUTURE: 선물, CONTEST: 대회용)", example = "SPOT")
+        @Schema(
+                description = "참가할 콘텐츠 타입 (MOCK: 모의투자, TRADE_SPOT: 실전 현물, TRADE_FUTURE: 실전 선물, CONTEST: 대회 선물)",
+                example = "TRADE_SPOT"
+        )
         AssetType assetType
 ){}
