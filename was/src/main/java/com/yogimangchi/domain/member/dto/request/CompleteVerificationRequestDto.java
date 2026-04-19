5faad6433d7eab792aa194fcd4d1885916633caa
KimJeongJin <wjdwls8520@gmail.com>
2026-04-19 13:33:04 +0900

    [Server] 멤버정보 주소 길이제한 255로 변경

--- was/src/main/java/com/yogimangchi/domain/member/dto/request/CompleteVerificationRequestDto.java
+++ was/src/main/java/com/yogimangchi/domain/member/dto/request/CompleteVerificationRequestDto.java
@@ -12,9 +12,9 @@ public record CompleteVerificationRequestDto(
     @NotBlank @Size(max = 5)
     String addressCode,
 
-    @NotBlank @Size(max = 20)
+    @NotBlank @Size(max = 255)
     String address1,
 
-    @Size(max = 100)
+    @Size(max = 255)
     String address2
 ) {}
