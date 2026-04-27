ac243c3ac29f298fba69dbdfff640f82e123caee
geunyoung <hms09131@gmail.com>
2026-04-27 17:52:49 +0900

    [SEVER] 새게시글 ux이벤트를 위한 registry,enum,dto 추가

---
+++ was/src/main/java/com/yogimangchi/global/sse/enums/CommunityUxSseEventType.java
@@ -0,0 +1,9 @@
+package com.yogimangchi.global.sse.enums;
+
+public enum CommunityUxSseEventType {
+    // UX SSE 구독 직후 연결 성공 여부를 확인할 때
+    CONNECTED,
+
+    // 커뮤니티 피드에 새 게시글이 생겼음을 알릴 때
+    UX_COMMUNITY_POST_CREATED
+}
