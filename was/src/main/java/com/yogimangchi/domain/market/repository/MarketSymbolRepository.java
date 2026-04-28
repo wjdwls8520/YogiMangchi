b1d3b2aae5fab9e76986c92a97dc531a506d52ea
KimJeongJin <wjdwls8520@gmail.com>
2026-04-28 14:54:05 +0900

    [Server] 거래가능 선물 심볼 조회 api 제작

--- was/src/main/java/com/yogimangchi/domain/market/repository/MarketSymbolRepository.java
+++ was/src/main/java/com/yogimangchi/domain/market/repository/MarketSymbolRepository.java
@@ -1,10 +1,13 @@
 package com.yogimangchi.domain.market.repository;
 
+import com.yogimangchi.domain.market.dto.response.MarketSymbolResponseDto;
 import com.yogimangchi.domain.market.entity.MarketSymbol;
 import com.yogimangchi.domain.market.enums.MarketType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 import java.util.List;
 
 public interface MarketSymbolRepository extends JpaRepository<MarketSymbol, String> {
@@ -12,8 +15,27 @@ public interface MarketSymbolRepository extends JpaRepository<MarketSymbol, Stri
     // 전체조회
     List<MarketSymbol> findAllByIsActiveTrue();
 
-    // 현물, 선물 특적 코인 + 공통 코인 조회
+    // 현물
     @Query("SELECT m FROM MarketSymbol m WHERE m.isActive = true AND m.marketType IN (:type, 'BOTH')")
     List<MarketSymbol> findActiveSymbolsByType(@Param("type") MarketType type);
+
+    // 선물
+    @Query("""
+        SELECT new com.yogimangchi.domain.market.dto.response.MarketSymbolResponseDto(
+            fsp.symbol,
+            ms.symbol,
+            ms.displayNameKr,
+            ms.displayNameEn,
+            ms.baseAsset,
+            ms.quoteAsset
+        )
+        FROM FuturesSymbolPolicy fsp
+        JOIN fsp.marketSymbol ms
+        WHERE ms.isActive = true AND ms.marketType = 'BOTH'
+    """)
+    List<MarketSymbolResponseDto> findActiveFuturesSymbolsByType();
+
+
+
 
 }
